6dcb3e3bac7a6f34294f5ea5de7599106dddd294
Rhesa Febrin Saputra <rhesa.saputra@gmail.com>
2018-08-15 12:16:51 +0700

    Add LobbyViewController & GameplayViewController

---
+++ MC 3/Controller/GameplayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameplayViewController.swift
+//  MC 3
+//
+//  Created by Rhesa Febrin Saputra on 8/15/18.
+//  Copyright © 2018 Yosua Hoo. All rights reserved.
+//
+
+import UIKit
+
+class GameplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
