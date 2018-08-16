dd8b553e93f0d8ade09f8fff29fb0b834a4d5ea7
Yosua Hoo <yosu4.hoo@gmail.com>
2018-08-16 16:15:36 +0700

    add Gameplay View Controller

---
+++ MC 3/Controller/GameplayViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameplayViewController.swift
+//  MC 3
+//
+//  Created by Yosua Hoo on 15/08/18.
+//  Copyright © 2018 Yosua Hoo. All rights reserved.
+//
+
+import UIKit
+
+class GameplayViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
