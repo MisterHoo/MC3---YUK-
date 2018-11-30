57e954b13ec99f06c74de9cc9cdfcf845bfb8e72
Kennyzi Yusuf <ykennyzi@gmail.com>
2018-11-30 14:28:17 +0700

    assset Tutorial solo

--- MC 3/Controller/Tentang Controller/ExplainViewController.swift
+++ MC 3/Controller/Tentang Controller/ExplainViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ExplainViewController: UIViewController {
+class ExplainViewController: UIViewController, UIScrollViewDelegate {
+    
     var text = 0
+    var imageArray = [UIImage]()
     @IBOutlet weak var mainScrollView: UIScrollView!
     @IBOutlet weak var cobaLabel: UILabel!
-    var imageArray = [UIImage]()
+    @IBOutlet weak var pageControll: UIPageControl!
+    
+    var tutorialText = ["satu","dua","tiga","empat", "lima"]
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        mainScrollView.frame = view.frame
+        pageControll.layer.cornerRadius = 10
+//        mainScrollView.frame = view.frame
         switch text {
         case 1:
-            imageArray = [#imageLiteral(resourceName: "BG Splash & Main Menu - ungu"),#imageLiteral(resourceName: "BG Duo"),#imageLiteral(resourceName: "BG Solo")]
+            imageArray = [#imageLiteral(resourceName: "1p step 1"),#imageLiteral(resourceName: "1p step 2"),#imageLiteral(resourceName: "1p step 3"),#imageLiteral(resourceName: "1p step 4"),#imageLiteral(resourceName: "1p step 5")]
         case 2:
             imageArray = [#imageLiteral(resourceName: "BG Duo"),#imageLiteral(resourceName: "BG Splash & Main Menu - ungu"),#imageLiteral(resourceName: "BG Solo Ipad")]
         case 3:
@@ -32,15 +37,45 @@ class ExplainViewController: UIViewController {
             let imageView = UIImageView()
             imageView.image = imageArray[i]
             imageView.contentMode = .scaleAspectFit
-            let xPosition = self.view.frame.width * CGFloat(i)
+            let xPosition = mainScrollView.frame.width * CGFloat(i)
             imageView.frame = CGRect(x: xPosition, y: 0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
             
             mainScrollView.contentSize.width = mainScrollView.frame.width * CGFloat(i + 1)
             mainScrollView.addSubview(imageView)
         }
+        pageControll.numberOfPages = imageArray.count
+        self.mainScrollView.delegate = self
         let labelTulis = String(text)
         cobaLabel.text = labelTulis
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControll.currentPage = Int(pageNumber)
+        
+        switch pageNumber
+        {
+        case 0:
+            cobaLabel.text = tutorialText[0]
+            break
+        case 1:
+            cobaLabel.text = tutorialText[1]
+            break
+        case 2:
+            cobaLabel.text = tutorialText[2]
+            break
+        case 3:
+            cobaLabel.text = tutorialText[3]
+            break
+        case 4:
+            cobaLabel.text = tutorialText[4]
+            break
+        default:
+            break
+        }
+        
+    }
+
 
     @IBAction func backToTentang(_ sender: Any) {
         dismiss(animated: true, completion: nil)
